77560e1fb0e523a9d78ab78094bf1faa05590a4d
Isadora Coutinho <62269694+IsadoraCoutinho@users.noreply.github.com>
2023-01-02 16:12:21 -0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+{
+    question: "Quanto é 1 + 2?",
+    answers: [
+        {option:"3", corretc: true},
+        {option:"2", corretc: false},
+        {option:"4", corretc: false},
+    ],
+},
+{
+    question: "Quanto é 5 x 2?",
+    answers: [
+        {option:"9", corretc: false},
+        {option:"10", corretc: true},
+        {option:"11", corretc: false},
+    ],
+},
+{
+    question: "Quanto é 10 x 8?",
+    answers: [
+        {option:"800", corretc: false},
+        {option:"90", corretc: false},
+        {option:"80", corretc: true},
+    ],   
+},
+{
+    question: "Quanto é 7 x 7?",
+    answers: [
+        {option:"70", corretc: false},
+        {option:"56", corretc: false},
+        {option:"49", corretc: true},
+    ],
+},
+];
